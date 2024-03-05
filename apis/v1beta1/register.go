88a6a062fcf42b75e214d95527f2b100642760b9
Marcel Rieger <marcel-pierre.rieger@bmw.de>
2024-03-05 09:46:31 +0000

    checkpoint: preparation

--- apis/v1beta1/register.go
+++ apis/v1beta1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "github.abc.de"
 	Version = "v1beta1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/mriegr/provider-github
 
 go 1.19
 
